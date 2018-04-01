4ff97a8281a8797485837d283176b24c42a3d5dd
Thanos_Ad <de.bomb1789@gmail.com>
2018-04-01 17:39:35 +0300

    Update AI_Test.java

--- AI_Test.java
+++ AI_Test.java
@@ -6,7 +6,7 @@ public class AI_Test {
 		Maze.CreateMaze();                                          // Create the Maze
 		System.out.println(Maze.toString());                        // Print the Maze
 		PathFinder PathG1 = new PathFinder(0,0,13,10,Maze);         // Set the Staring Point to (0,0) and the Destination to (13,10)
-		PathG1.ManchattanDistanceCalculator();                      // Calculate The Manchattan Distance of all nodes
+		PathG1.MachattanDistanceCalculator();                       // Calculate The Manhattan Distance of all nodes
 		PathG1.GCalculator();                                       // Calculate the G cost and the final path
 		ArrayList<Node> PathToG1 = PathG1.printPath();              // Print the shortest path from S to G
 	}
